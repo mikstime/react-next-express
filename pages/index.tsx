f49c342a6df22754ca222dd378bbc85a00d12daf
MBtsky <9250499222@mail.ru>
2021-06-15 22:00:56 +0300

    types updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import Page from '../components/page'
+import {GetServerSideProps} from 'next'
 
-interface StatelessPage<P = {}> extends React.FC<P> {
-    getInitialProps?: (ctx: any) => Promise<P>
-}
-
-const FormsApp: StatelessPage = () => {
+const FormsApp: React.FC = () => {
     return <Page/>
 }
 
-export const getServerSideProps = async ({req}) => {
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
     const res = await fetch(`${process.env.API}/store`, {
         headers: {cookie: req.headers.cookie}
     })
     const response = await res.json()
     let store = {market: {price: 0, quantity: 0, total: 0, order: [0, 1, 2]}}
-    // init store if value was found
+    // change store if value was found
     if (response) store.market = {...store.market, ...response}
 
     return {props: {initialReduxState: store}}
 }
+
 export default FormsApp
