dc4db324942c1c4caf4179293d6a08d312d42566
MBtsky <9250499222@mail.ru>
2021-06-15 21:59:21 +0300

    getInitialProps changed to getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Page from '../components/page'
-import {NextPageContext} from 'next'
-import { ServerStyleSheet } from 'styled-components'
 
 interface StatelessPage<P = {}> extends React.FC<P> {
     getInitialProps?: (ctx: any) => Promise<P>
@@ -11,29 +9,15 @@ const FormsApp: StatelessPage = () => {
     return <Page/>
 }
 
-FormsApp.getInitialProps = async ({req}: NextPageContext) => {
+export const getServerSideProps = async ({req}) => {
     const res = await fetch(`${process.env.API}/store`, {
-        headers: req ? {cookie: req.headers.cookie} : undefined
+        headers: {cookie: req.headers.cookie}
     })
-
-    let store = {market: {price: 0, quantity: 0, total: 0, order: [0, 1, 2]}}
-
     const response = await res.json()
-    if (response) {
-        const {quantity, total, price} = response
-        store.market = {...store.market, quantity, total, price}
-    }
+    let store = {market: {price: 0, quantity: 0, total: 0, order: [0, 1, 2]}}
+    // init store if value was found
+    if (response) store.market = {...store.market, ...response}
 
-    const sheet = new ServerStyleSheet()
-    // prevent screen from blinking during page load
-    try {
-        sheet.collectStyles(<FormsApp/>)
-    } catch (error) {
-        // nop
-    } finally {
-        sheet.seal()
-    }
-    return {initialReduxState: store}
+    return {props: {initialReduxState: store}}
 }
-
 export default FormsApp
